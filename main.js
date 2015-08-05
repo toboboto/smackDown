255ba977776505513f1b935d7235fd8bb20efd15
Howard Chen <chenh0886@gmail.com>
2015-08-05 11:38:16 -0700

    Apple Rss with Wikipedia

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
     $("#getHits").click(function(){
         $.ajax({
             dataType: 'json',
-            url: 'https://itunes.apple.com/us/rss/topsongs/limit=10/genre=1007/json',
+            url: 'https://itunes.apple.com/us/rss/topsongs/limit=20/genre=1007/json',
             success: function(result){
                 console.log('loaded',result);
                 global_result = result;
@@ -27,41 +27,48 @@ $(document).ready(function(){
                     var h3Title = $("<h3>",{
                         text: musicFind[i]["im:name"].label,
                     });
-                    var pDirector = $("<p>",{
+                    var pArtist = $("<p>",{
                         text: musicFind[i]["im:artist"].label,
                     });
                     var img = $("<img>", {
                         src: thirdImage,
                     });
                     var musicContainer = $("<div>",{
-                        class: "col-xs-4",
+                        class: "col-xs-4 artist",
                     });
-                    musicContainer.append(h3Title, pDirector, img)
+                    musicContainer.append(h3Title, pArtist, img)
                     $body.append(musicContainer);
+
+                    (function(){
+                        $(musicContainer).click(function(){
+                            var artist = $(this).find("p").text();
+                            var request = artist.replace(/ /g, "_");
+                            $.ajax({
+                                type: "GET",
+                                url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page="+request+"&callback=?",
+                                contentType: "application/json; charset=utf-8",
+                                async: false,
+                                dataType: "jsonp",
+                                success: function (data, textStatus, jqXHR) {
+                                    $(".dummyLayout").html(data.parse.text["*"]);
+                                    var infobox = $(".infobox tbody");              //THIS ARRAY HOLDS ALL THE INFO WE NEED FROM WIKIPEDIA!!!
+                                    console.log("This is infobox",infobox[0]);      //Test console logging the zero index of this array
+                                },
+                                error: function (errorMessage) {
+                                }
+                            });
+                        });
+                    })();
                 }
             }
         });
     });
 
-    $("#go").click(function(){
-        var request = "Black_Sabbath"
-        $.ajax({
-            type: "GET",
-            url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page="+request+"&callback=?",
-            contentType: "application/json; charset=utf-8",
-            async: false,
-            dataType: "jsonp",
-            success: function (data, textStatus, jqXHR) {
-                console.log(data);
-                test = data;
-                $(".dummyLayout").html(data.parse.text["*"]);
-                var thing = $(".infobox tbody");
-                console.log(thing);
 
-            },
-            error: function (errorMessage) {
-            }
+    $(".artist").click(function(){
+        var artist = $(this).find("p").text;
+        console.log("yay");
+        var request = "Father_John_Misty";
 
-        });
     });
 });
