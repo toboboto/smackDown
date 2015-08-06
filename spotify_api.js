0aadd35f110f90545ee7c333c3a3c5cf271e10bf
JEkstroms <jmekstrom@gmail.com>
2015-08-05 18:24:08 -0700

    preview play working

--- spotify_api.js
+++ spotify_api.js
@@ -7,6 +7,8 @@ var artistSpotifyImgURL;
 var albumSpotifyID;
 var albumSpotifyObj;
 var albumSpotifyImgURL;
+var spotifyAlbum;
+var spotifyTrackURI;
 var search_value = "";
 
 var searchArtists = function (query) {
@@ -23,7 +25,7 @@ var searchArtists = function (query) {
             console.log(artistSpotifyObj);
             artistSpotifyID = artistSpotifyObj.artists.items[0].id;
             console.log(artistSpotifyID);
-            artistSpotifyImgURL = artistSpotifyObj.artists.items[0].images[1].url;
+            artistSpotifyImgURL = artistSpotifyObj.artists.items[0].images[0].url;
             console.log(artistSpotifyImgURL);
             $(".dummyLayout").append("<img src='"+artistSpotifyImgURL+"'>").show();
             console.log(search_value);
@@ -42,20 +44,38 @@ var searchAlbums = function (query) {
         success: function (response) {
             //  resultsPlaceholder.innerHTML = template(response);
             albumSpotifyObj = response;
-            console.log(albumSpotifyObj);
-
+            albumId = albumSpotifyObj.albums.items[0].id;
+            console.log(albumId);
+            getTrackURI(albumId);
+            //$(".spotify_player").attr("src","https://embed.spotify.com/?uri=spotify:album:"+ albumId);
         }
     });
 };
 
-var getTrackURI = function (albumId, callback) {
+var getTrackURI = function (albumId) {
     $.ajax({
         url: 'https://api.spotify.com/v1/albums/' + albumId,
         success: function (response) {
-            callback(response);
+            spotifyAlbum = response;
+            console.log("album",spotifyAlbum);
+            spotifyTrackURI = spotifyAlbum.tracks.items[0].uri;
+            console.log(spotifyTrackURI);
+            $(".spotify_player").attr("src","https://embed.spotify.com/?uri="+ spotifyTrackURI);
+            var preview_link = spotifyAlbum.tracks.items[0].preview_url;
+            var test = new Audio(preview_link);
+            test.play();
+
+            (function() {
+                $(".clickable").trigger('click');
+            })();
         }
     });
 };
+
+function play(){
+
+    console.log("play");
+}
 
 
 $(document).ready(function(){
