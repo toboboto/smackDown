89a4326881ca9b375b9e824982f021256f173680
Thomas F <toboboto@gmail.com>
2015-08-05 17:56:27 -0700

    modal frame up

---
+++ tobo.js
@@ -0,0 +1,8 @@
+
+
+
+var ttl = $('<p>',{text:global_title_array[i]});
+var auth= $('<p>',{text:global_author_array[i]});
+console.log (global_author_array[i], 'is the global title at',i);
+auth.appendTo('#main'); // append author to document
+ttl.appendTo('#main'); // append title to document
