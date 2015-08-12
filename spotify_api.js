dfbb85cc94039ba1afc7fd2088e635ccfea86389
Howard Chen <chenh0886@gmail.com>
2015-08-12 10:18:52 -0700

    Move bg to separate div. Filter will apply exclusively to background and not its contents

--- spotify_api.js
+++ spotify_api.js
@@ -32,10 +32,10 @@ var searchArtists = function (query) {
             //console.log(artistSpotifyImgURL);
             artistSpotifyURI = artistSpotifyObj.artists.items[0].uri;
             $(".spotify_follow").attr("src","https://embed.spotify.com/follow/1/?uri="+ artistSpotifyURI);
-            $(".artistModal").css({
+            $(".artistBg").css({
                 "background-image": "url(" +artistSpotifyImgURL + ")",
                 "background-repeat": "no-repeat",
-                "background-color": "black",
+                "background-color": "#4a4a4a",
                 "background-size": "100%"
             });
             //console.log(search_value);
